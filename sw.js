2676acdd100530aeb186586ff353d4739a7d8b15
YUMIKO NAKAGIRI <nakagiri3019@gmail.com>
2026-06-24 14:03:51 +0900

    [fix] PDFプレビュー日本語文字化け修正 cMapUrl追加・SW v24

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 ﻿const CACHE_PREFIX = 'mail-nakagiri-cache-';
-const CACHE_NAME = CACHE_PREFIX + 'v23';
+const CACHE_NAME = CACHE_PREFIX + 'v24';
 const PRECACHE_URLS = [
   './mail_nakagiri.html',
   './mail_nakagiri2.html',
