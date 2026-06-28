1f4111bfe45c00cb3f7e65601abcb785e986cdd2
YUMIKO NAKAGIRI <nakagiri3019@gmail.com>
2026-06-29 01:55:32 +0900

    fix: 午前午後表記を24時間制に変換しソート修正

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 ﻿const CACHE_PREFIX = 'mail-nakagiri-cache-';
-const CACHE_NAME = CACHE_PREFIX + 'v34';
+const CACHE_NAME = CACHE_PREFIX + 'v35';
 const PRECACHE_URLS = [
   './mail_nakagiri.html',
   './mail_nakagiri2.html',
