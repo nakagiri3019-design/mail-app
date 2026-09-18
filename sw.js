1d71622ca76db14f077a83cd4d6f28a4f3264278
YUMIKO NAKAGIRI <nakagiri3019@gmail.com>
2026-09-18 18:29:59 +0900

    エールジャパン請求書メールの時刻を修正

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 ﻿const CACHE_PREFIX = 'mail-nakagiri-cache-';
-const CACHE_NAME = CACHE_PREFIX + 'v73';
+const CACHE_NAME = CACHE_PREFIX + 'v74';
 const PRECACHE_URLS = [
   './',
   './index.html',
